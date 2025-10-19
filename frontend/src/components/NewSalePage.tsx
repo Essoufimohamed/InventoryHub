65cc047026ee01fdcd7e4e383f7138819c8c785b
mohamed essoufi <essoufimohamed18@gmail.com>
2025-10-19 02:55:21 +0100

    fix: redesign the product card and shopping card

--- frontend/src/components/NewSalePage.tsx
+++ frontend/src/components/NewSalePage.tsx
@@ -1,107 +1,23 @@
 // NewSalePage.tsx
-import React, { useContext, useState } from "react";
 import ProductCategoryFilter from "./ProductCategoryFilter";
 import ProductSearchBar from "./ProductSearchBar";
 import ProductGrid from "./ProductGrid";
 import ShoppingCart from "./ShoppingCart";
 
-import img from "../assets/watch.jpg";
-import { ProductContext, ProductProvider } from "../context/ProductContext";
+import { ProductProvider } from "../context/ProductContext";
 
-// Define your Product and CartItem types
-interface Product {
-    id: string;
-    image: string;
-    title: string;
-    price: number;
-    stock: number;
-}
-
-interface CartItem {
-    id: string;
-    title: string;
-    price: number;
-    quantity: number;
-}
-
-const DUMMY_PRODUCTS: Product[] = [
-    {
-        id: "1",
-        image: img,
-        title: "title loremm jdjdvhkdjk",
-        price: 250,
-        stock: 100,
-    },
-    {
-        id: "2",
-        image: img,
-        title: "title loremm jdjdvhkdjk",
-        price: 250,
-        stock: 10,
-    },
-    {
-        id: "3",
-        image: img,
-        title: "title loremm jdjdvhkdjk",
-        price: 250,
-        stock: 100,
-    },
-    {
-        id: "4",
-        image: img,
-        title: "title loremm jdjdvhkdjk",
-        price: 250,
-        stock: 10,
-    },
-    {
-        id: "5",
-        image: img,
-        title: "title loremm jdjdvhkdjk",
-        price: 250,
-        stock: 100,
-    },
-    {
-        id: "6",
-        image: img,
-        title: "title loremm jdjdvhkdjk",
-        price: 250,
-        stock: 10,
-    },
-    {
-        id: "7",
-        image: img,
-        title: "title loremm jdjdvhkdjk",
-        price: 250,
-        stock: 100,
-    },
-];
-
-// const CATEGORIES = ["Beauty", "Gadget", "Electronics"];
-
-const NewSalePage: React.FC = () => {
-    const [searchQuery, setSearchQuery] = useState("");
-
+const NewSalePage = () => {
     return (
         <ProductProvider>
             <div className="flex gap-3">
                 <div className="flex-grow">
                     <div className="flex items-center space-x-4 mb-4">
                         <ProductCategoryFilter />
-                        <ProductSearchBar
-                            value={searchQuery}
-                            onSearch={setSearchQuery}
-                        />
+                        <ProductSearchBar />
                     </div>
-                    <ProductGrid
-                    // products={filteredProducts}
-                    // onAddToCart={handleAddToCart}
-                    />
+                    <ProductGrid />
                 </div>
-                <ShoppingCart
-                // cartItems={cart}
-                // totalAmount={calculateTotal()}
-                // onCheckout={handleCheckout}
-                />
+                <ShoppingCart />
             </div>
         </ProductProvider>
     );
--- frontend/src/components/ProductGrid.tsx
+++ frontend/src/components/ProductGrid.tsx
@@ -1,36 +1,3 @@
-// // ProductGrid.tsx
-// import React from "react";
-// import ProductCard from "./ProductCard"; // Assuming ProductCard is in the same directory
-
-// interface Product {
-//     id: string;
-//     image: string;
-//     title: string;
-//     price: number;
-//     stock: number;
-// }
-
-// interface ProductGridProps {
-//     products: Product[];
-//     onAddToCart: (productId: string, quantity: number) => void;
-// }
-
-// const ProductGrid: React.FC<ProductGridProps> = ({ products, onAddToCart }) => {
-//     return (
-//         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4 flex-grow pr-4">
-//             {products.map((product) => (
-//                 <ProductCard
-//                     key={product.id}
-//                     product={product}
-//                     onAddToCart={onAddToCart}
-//                 />
-//             ))}
-//         </div>
-//     );
-// };
-
-// export default ProductGrid;
-
 // ProductList.jsx
 import React, { useContext } from "react";
 import ProductCard from "./ProductCard";
